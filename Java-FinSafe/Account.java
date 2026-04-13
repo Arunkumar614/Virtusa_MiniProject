510ad57e4f5b8ed3737f86724ebbcac969fc0451
Arunkumar614 <2303a51l48@sru.edu.in>
2026-04-13 12:24:51 +0530

    Created required Private Variables and created deposit method also handled invalid amount like less than 0 exception

---
+++ Java-FinSafe/Account.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Account {
+    private String accountHolder;
+    private double balance;
+    private ArrayList<Double> transactions =new ArrayList<>();
+    public Account(String accountHolder,double balance){
+        this.accountHolder=accountHolder;
+        this.balance=balance;
+    }
+    public void deposit(double amount){
+        if(amount<=0){
+            throw new IllegalArgumentException("Invalid deposit amount");
+        }
+        balance+=amount;
+        transactions.add(amount);
+    }
+
+
+
+}
